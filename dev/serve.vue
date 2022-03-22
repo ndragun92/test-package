a5a88bf34671f1178c36ed3ce7f76046a39e7bb6
ndragun <39500267+ndragun92@users.noreply.github.com>
2022-03-22 23:33:27 +0100

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { CadenzaDemoSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  CadenzaDemoSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <cadenza-demo-sample />
+    <cadenza-demo-button />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as CadenzaDemoSample } from './CadenzaDemoSample/CadenzaDemoSample.vue';
+export { default as CadenzaDemoButton } from './CadenzaDemoButton/CadenzaDemoButton.vue';
